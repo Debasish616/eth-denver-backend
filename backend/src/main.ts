ed8a206caaafe4288d05c63802a027c23b351ce9
Prathamesh Mutkure <pmutkure009@gmail.com>
2025-03-01 18:30:54 +0530

    Swagger Docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
 
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Arbitrage Service')
+    .setDescription('The Arbitrage Service API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
 
